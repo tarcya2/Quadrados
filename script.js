85f9c466cb40c7c0796b5288e5adb993d9ac3b8e
Gabriel Tarcia <gabriel.liminha2025@gmail.com>
2026-02-21 15:48:00 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const container = document.getElementById ("container");
+for (let i = 0; i < 10; i++) {
+    const square = document.createElement ("div");
+    square.classList.add ("square");
+    container.appendChild (square);
+}
